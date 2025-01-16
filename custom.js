be0e6fc45925079d4995c656b9b6f2c8a6d9e82e
Sean Oneil Rosario <seanrosario119@gmail.com>
2025-01-16 16:58:11 +0800

    test

--- custom.js
+++ custom.js
@@ -1,12 +1,14 @@
 window.onscroll = function() {
     // Get the scroll height
-    var scrollHeight = document.documentElement.scrollHeight;
+    // var scrollHeight = document.documentElement.scrollHeight;
     // Get the current scroll position
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     // Get the client height (viewport height)
-    var clientHeight = document.documentElement.clientHeight;
+    // var clientHeight = document.documentElement.clientHeight;
 
     if (scrollTop > 65) {
-        console.log("!!!")
+        document.querySelector("#section-header").classList.add("scrolled")
+    } else {
+        document.querySelector("#section-header").classList.remove("scrolled")
     }
 };
